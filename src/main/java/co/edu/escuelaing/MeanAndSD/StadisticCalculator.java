ecfd475c9a182a43f385329fd2e76b3b17770d89
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-06-09 12:36:08 -0500

    Media implementada

--- src/main/java/co/edu/escuelaing/MeanAndSD/StadisticCalculator.java
+++ src/main/java/co/edu/escuelaing/MeanAndSD/StadisticCalculator.java
@@ -4,10 +4,30 @@
  */
 package co.edu.escuelaing.MeanAndSD;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
- * @author cristian.forero-m
+ * @author cristian forero
  */
 public class StadisticCalculator {
+    
+    public static double mean(List<? extends Double> li){
+        double result = 0;
+        for(Double i:li){
+            result += i;
+        }
+        result /= li.size();
+        return result;
+    }
+    
+    public static void main ( String args[])
+    {
+        List<Integer> li = new LinkedList<Integer>();
+        li.add(0);
+        li.add(10);
+        System.out.println(mean(li));
+    }
     
 }
