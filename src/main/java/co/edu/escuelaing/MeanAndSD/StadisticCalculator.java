767e2739d0a9954ff58c69cad85cfe76857c733d
Cristian Forero <cristian.forero-m@mail.escuelaing.edu.co>
2022-06-10 16:00:10 -0500

    Entrada estandar implementada

--- src/main/java/co/edu/escuelaing/MeanAndSD/StadisticCalculator.java
+++ src/main/java/co/edu/escuelaing/MeanAndSD/StadisticCalculator.java
@@ -5,7 +5,9 @@
 package co.edu.escuelaing.MeanAndSD;
 
 import co.edu.escuelaing.LinkedList.MyLList;
+import java.io.File;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -45,12 +47,20 @@ public class StadisticCalculator {
         return result;
     }
     
-    public static void main ( String args[])
+    public static void main( String args[])throws Exception
     {
         List<Double> li = new MyLList<Double>();
-        li.add(0.0);
-        li.add(10.0);
-        li.add(3.5);
+        File file = new File( args[0] );
+        Scanner scan = new Scanner(file);
+        
+        while(scan.hasNextLine()) 
+        {
+            String line = scan.nextLine();
+            if(line != "")
+            {
+                li.add(Double.parseDouble(line.trim()));
+            }
+        }
         System.out.println(mean(li));
         System.out.println(standardDesviation(li));
     }
